b1c8289b05d9729245d97c47d30040fc58514742
CesarTllez <lcesartellezl@gmail.com>
2021-11-30 09:05:24 -0500

    Validar existencia de artista en actualizar

--- src/main/java/co/edu/unicundi/discotiendaejbjar/servicio/implementacion/ArtistaServicioImp.java
+++ src/main/java/co/edu/unicundi/discotiendaejbjar/servicio/implementacion/ArtistaServicioImp.java
@@ -20,16 +20,17 @@ import javax.persistence.TypedQuery;
 import org.modelmapper.ValidationException;
 
 /**
- * Clase que permite acceder a los métodos que operan la base de datos y
- * valida los datos que proporciona el api.
+ * Clase que permite acceder a los métodos que operan la base de datos y valida
+ * los datos que proporciona el api.
+ *
  * @author César Rodríguez
  * @author Eison Morales
  * @author Juan Páez
  * @author Diego Cobos
  */
 @Stateless
-public class ArtistaServicioImp implements IArtistaServicio{
-    
+public class ArtistaServicioImp implements IArtistaServicio {
+
     /**
      * Permite acceder a los métodos que operan la base de datos.
      */
@@ -37,98 +38,109 @@ public class ArtistaServicioImp implements IArtistaServicio{
     private IArtistaRep repositorio;
 
     /**
-     * Método que comprueba si el id existe, si es así, busca el artista
-     * con dicho id.
+     * Método que comprueba si el id existe, si es así, busca el artista con
+     * dicho id.
+     *
      * @param id
-     * @return 
+     * @return
      */
     @Override
-    public Artista buscarPorId(Integer id)throws ResourceNotFoundException{
-        if(this.repositorio.validarExistenciaPorId(id) == 1){
+    public Artista buscarPorId(Integer id) throws ResourceNotFoundException {
+        if (this.repositorio.validarExistenciaPorId(id) == 1) {
             return this.repositorio.buscarPorId(id);
-        }else{
+        } else {
             throw new ResourceNotFoundException("Ese id no existe en la base de datos.");
-           
+
         }
-    
+
     }
 
     /**
      * Método que busca a todos los artistas.
-     * @return 
+     *
+     * @return
      */
     @Override
     public List<Artista> buscarTodo() {
         return this.repositorio.buscarTodo();
     }
 
-      @Override
-    public List<Vista> vistaBuscar() {
-    return this.repositorio.vistaBuscar();
-    }
-    /**
-     * Método que comprueba si el nombre no están registrado con 
-     * otro artista, si es así, permite efectur el registro.
-     * @param objeto 
-     */
     @Override
-    public void registrar(Artista objeto) throws ResourceConflictException{
-        if(this.repositorio.validarExistenciaPorNombre(objeto.getNombre()) == 1){
-            throw  new ResourceConflictException("Actualmente hay un artista registrado con ese nombre.");
-            }else{
-                this.repositorio.registrar(objeto);
-            }
+    public List<Vista> vistaBuscar() {
+        return this.repositorio.vistaBuscar();
     }
 
     /**
-     * Método que comprueba si el id existe y el nombre no 
-     * está registrado con otro artista, si es así, permite modificar al artista.
-     * @param objeto 
+     * Método que comprueba si el nombre no están registrado con otro artista,
+     * si es así, permite efectur el registro.
+     *
+     * @param objeto
      */
     @Override
-    public void actualizar(Artista objeto) throws  EntityValidationException, ResourceNotFoundException, ResourceConflictException{
-        if((objeto.getId() != null)){
-            if(this.repositorio.validarExistenciaPorId(objeto.getId()) == 1){
-                if((!objeto.getNombre().equals(this.repositorio.buscarPorId(objeto.getId()).getNombre()))){
-                    this.repositorio.actualizar(objeto);
-                }else{
-                    throw  new ResourceConflictException("No ingreso un nombre diferente.");
-                }
-            }else{
-                throw  new ResourceNotFoundException("No existe ese id en la base de datos.");
-            }
-        }else{
-            throw  new EntityValidationException("Es necesario ingresar un id.");
+    public void registrar(Artista objeto) throws ResourceConflictException {
+        if (this.repositorio.validarExistenciaPorNombre(objeto.getNombre()) == 1) {
+            throw new ResourceConflictException("Actualmente hay un artista registrado con ese nombre.");
+        } else {
+            this.repositorio.registrar(objeto);
         }
     }
 
     /**
-     * Método que comprueba si el id ingresado existe, si es así, procede
-     * a eliminar al artista por dicho id.
-     * @param id 
+     * Método que comprueba si el id existe y el nombre no está registrado con
+     * otro artista, si es así, permite modificar al artista.
+     *
+     * @param objeto
      */
     @Override
-    public void eliminarJPQL(Integer id) throws ResourceNotFoundException{
-        if(this.repositorio.validarExistenciaPorId(id) == 1){
+    public void actualizar(Artista objeto) throws EntityValidationException, ResourceNotFoundException, ResourceConflictException {
+        if ((objeto.getId() != null)) {
+            if (this.repositorio.validarExistenciaPorId(objeto.getId()) == 1) {
+                if (this.repositorio.validarExistenciaPorNombre(objeto.getNombre()) == 1) {
+                    System.out.println("Excepcion: Ya existe ese artista.");
+                } else {
+                    if ((!objeto.getNombre().equals(this.repositorio.buscarPorId(objeto.getId()).getNombre()))) {
+                        this.repositorio.actualizar(objeto);
+                    } else {
+                        throw new ResourceConflictException("No ingreso un nombre diferente.");
+                    }
+                }
+            } else {
+                throw new ResourceNotFoundException("No existe ese id en la base de datos.");
+            }
+        } else {
+            throw new EntityValidationException("Es necesario ingresar un id.");
+        }
+    }
+
+    /**
+     * Método que comprueba si el id ingresado existe, si es así, procede a
+     * eliminar al artista por dicho id.
+     *
+     * @param id
+     */
+    @Override
+    public void eliminarJPQL(Integer id) throws ResourceNotFoundException {
+        if (this.repositorio.validarExistenciaPorId(id) == 1) {
             this.repositorio.eliminarJPQL(id);
-        }else{
+        } else {
             throw new ResourceNotFoundException("Excepcion: No existe ese id en la base de datos.");
         }
     }
 
     /**
-     * Método que comprueba si el id ingresado existe, si es así, procede
-     * a eliminar al artista por dicho id.
-     * @param id 
+     * Método que comprueba si el id ingresado existe, si es así, procede a
+     * eliminar al artista por dicho id.
+     *
+     * @param id
      */
     @Override
-    public void eliminarSQL(Integer id) throws ResourceNotFoundException{
-        if(this.repositorio.validarExistenciaPorId(id) == 1){
+    public void eliminarSQL(Integer id) throws ResourceNotFoundException {
+        if (this.repositorio.validarExistenciaPorId(id) == 1) {
             this.repositorio.eliminarSQL(id);
-        }else{
+        } else {
             throw new ResourceNotFoundException(" No existe ese id en la base de datos.");
 
         }
     }
-    
+
 }
